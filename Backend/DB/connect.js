c3ec4c137940ad1e9de06346b5bec6b153de10df
suryam205 <suryamkandapu205@gmail.com>
2025-05-02 15:57:34 +0530

    Updated MongoDB connection string and other changes

--- Backend/DB/connect.js
+++ Backend/DB/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/first")
+    await mongoose.connect(process.env.MONGO_URI)
      .then((e)=>console.log("mongoose connected successfully"))
      .catch((err)=>console.log("mongoose connection failed" , err));
 }
