c248632bdfea5b90aa31f92fca7487fa2d0bf138
Suryam <suryamkandapu205@gmail.com>
2025-05-14 02:54:20 +0530

    Update GetComments.jsx

--- Frontend/src/pages/Components/Comments/GetComments.jsx
+++ Frontend/src/pages/Components/Comments/GetComments.jsx
@@ -4,7 +4,7 @@ import { API_URL } from '../../Home';
 import "./Comment.css"
 
 
-const GetComments = ({contentId , contentType }) => {
+const GetComments = ({contentId , contentType , role }) => {
     const [comments , setComments] = useState([]);
 
     useEffect(()=>{
@@ -27,6 +27,24 @@ const GetComments = ({contentId , contentType }) => {
         handleGetComments();
     }, []);
 
+    
+    const handleDeleteComment = async(id)=>{
+        try{
+            const res = await axios.delete(`${API_URL}/comment/deleteComment/${id}`);
+            if(res.data.success){
+                alert("Comment deleted!")
+            }
+            else{
+                alert("Try another time")
+            }
+        }catch(err){
+            console.error("Error deleting comment:", err);
+            alert("Internal Error occurred");
+        }
+    }
+      
+    console.log(role);
+
   return (
     <div className="comment-section">
         <h3 className="comment-title">Your thoughts are the real trend- Share them below</h3>
@@ -36,8 +54,13 @@ const GetComments = ({contentId , contentType }) => {
         ) : (
             comments.map((comment) => (
             <div key={comment._id} className="comment-card">
+                
                 <strong className="comment-author">{comment.userId.fullName}</strong>
                 <p className="comment-text">{comment.text}</p>
+                {role === "vendor" &&(
+                     <button className="delete-comment" onClick={() => handleDeleteComment(comment._id)}>X</button>
+                )}
+                
             </div>
             ))
         )}
@@ -46,3 +69,4 @@ const GetComments = ({contentId , contentType }) => {
 }
 
 export default GetComments
+
