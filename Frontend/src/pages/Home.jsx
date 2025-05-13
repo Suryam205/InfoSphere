6c3177af9ea7561c6b7656b39b51e4c722ee6109
Suryam <suryamkandapu205@gmail.com>
2025-05-13 22:24:36 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import SportCard from './Components/SportsComponents/SportCard';
 import ProductCard from './Components/ShoppingComponents/ProductCard';
