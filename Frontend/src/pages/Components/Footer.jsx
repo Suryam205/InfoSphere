a2c3c75e5359633a3ed63cd9b0fc24e08a93acd3
Suryam <suryamkandapu205@gmail.com>
2025-05-14 15:55:07 +0530

    Update Footer.jsx

--- Frontend/src/pages/Components/Footer.jsx
+++ Frontend/src/pages/Components/Footer.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
+import About from './About'
+import Contact from './Contact'
 
 const Footer = () => {
   return (
     <div>
-        <div className="about-us" id="about">
-          <h1>About Us</h1>
-          <p>We are a team of passionate developers dedicated to creating amazing web applications.</p>
-          <p>Our mission is to provide the best user experience and deliver high-quality products.</p>
-          <p>Contact us for any inquiries or collaborations!</p>
-        </div>
-        <div className="contact-us" id="contact">
-          <h1>Contact Us</h1>
-          <p>If you have any questions or feedback, feel free to reach out to us!</p>
-          <p>Email:</p>
-          </div>
+        
+        <About/>
+        <Contact/>
         <div className="footer">
           <p>&copy; 2023 Surya's Web. All rights reserved.</p>
           <p>Follow us on social media: Facebook | Twitter | Instagram</p>
