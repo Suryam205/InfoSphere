13dc732bfef2f456697389b920f782192b646588
Suryam <suryamkandapu205@gmail.com>
2025-05-15 00:37:56 +0530

    Update Navbar.jsx

--- Frontend/src/pages/Components/Navbar.jsx
+++ Frontend/src/pages/Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({role}) => {
       <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="/home">Surya's Web</Link>
+            <Link className="navbar-brand" to="/home">InfoSphere</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
