ad6d7b60693777e1e0d50d3a514807059d5f42bd
Suryam <suryamkandapu205@gmail.com>
2025-05-14 15:54:41 +0530

    Update Contact.jsx

--- Frontend/src/pages/Components/Contact.jsx
+++ Frontend/src/pages/Components/Contact.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1>Contact page</h1>
+       <div className="contact-us" id="contact">
+          <h1>Contact Us</h1>
+          <p>If you have any questions or feedback, feel free to reach out to us!</p>
+          <p>Email:</p>
+          </div>
     </div>
   )
 }
